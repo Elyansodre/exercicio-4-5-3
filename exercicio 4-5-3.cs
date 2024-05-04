ad943a4884544ff9d0bff5af7ac21bfee20765d6
Elyansodre <128232034+Elyansodre@users.noreply.github.com>
2024-05-04 16:10:34 -0300

    Add files via upload

---
+++ exercicio 4-5-3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 4;
+
+            while (x < 3)
+            {
+                y = y + 2;
+                x = x + 1;
+                Console.WriteLine(x + "-" + y);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
